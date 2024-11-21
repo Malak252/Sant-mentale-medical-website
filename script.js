c1b3bf72cc254c5845bd61196b243867520f9bc7
TheCoolestMalak <71954659+Malak252@users.noreply.github.com>
2024-11-21 23:49:31 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("contactForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    alert("Merci pour votre message! Nous vous répondrons bientôt.");
+});
+
+document.getElementById("startButton").addEventListener("click", function () {
+    alert("Welcome to your free trial! Let's get started.");
+});
